0c4038a10b243446972f9ffe39c2f74ffe5fbb1e
WakingHours-GitHub <WakingHoursHUC@outlook.com>
2023-03-02 15:24:55 +0800

    update

---
+++ numpy/1_简介.py
@@ -0,0 +1,27 @@
+"""
+numpy数据类型:
+    numpy数据类型有很多
+
+    numpy的数值类型实际上是dtype对象的实例, 并对应唯一的字符, 包括np.bool_, np.int32, np.float32.等.
+    
+
+
+
+
+
+
+
+
+
+"""
+
+
+import numpy as np
+
+
+
+
+dt = np.dtype(np.int32)
+
+print(dt)
+print(type(dt))
